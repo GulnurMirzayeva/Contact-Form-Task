f942f531356e2eaeaddcb0b4968fdbddc55c6fff
GulnurMirzayeva <gmirzyeva1@gmail.ru>
2025-07-01 15:12:56 +0400

    Last changes are ready

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -64,7 +64,7 @@
                     @if(session('success'))
                         <div id="successAlert" class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded mb-4 relative transition-all duration-300">
                             <span class="block sm:inline">{{ session('success') }}</span>
-                            <button onclick="closeAlert()" class="absolute top-0 right-0 mt-2 mr-2 text-green-700 hover:text-green-900 focus:outline-none">
+                            <button onclick="closeAlert()" class="absolute top-2 right-0 mt-2 mr-2 text-green-700 hover:text-green-900 focus:outline-none">
                                 <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 20 20">
                                     <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
                                 </svg>
